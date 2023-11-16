7e7205b680f1ef0bf3c074ea864f34b42686cf41
Dromance <88290916+pflaquer@users.noreply.github.com>
2023-11-16 11:48:15 -0500

    Create users.js

---
+++ users.js
@@ -0,0 +1,13 @@
+onst express = require('express');
+
+const router = express.Router();
+
+const {register} = require("../controllers/register");
+
+const {login} = require("../controllers/login");
+
+router.post('/register' , register); //POST request to register the user
+
+router.post('/login' , login); // POST request to login the user
+
+module.exports = router;
