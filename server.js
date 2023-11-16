effee3b4e2ea4df8d2f9de071ef1017fa490766f
Dromance <88290916+pflaquer@users.noreply.github.com>
2023-11-16 11:48:01 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const  express  =  require("express");
 require("./configs/dotenv");
 const  client  =  require("./configs/database");
+const  user  =  require("./routes/user");
+
+
+
 
 const app = express(); //Initialized express
 
 app.use(express.json());
 app.use(cors());
+
 
 const port = process.env.PORT || 5000;
 
@@ -14,12 +19,15 @@ app.get("/", (req, res) => {
 res.status(200).send("Engine Started, Ready to take off!");
 
 })
+app.use("/user",  user);  //Route for /user endpoint of API
 
 app.listen(port, () => {
 
 console.log(`Here we go, Engines started at ${port}.`);
 
 })
+
+
 
 
 client.connect((err) => { //Connected Database
